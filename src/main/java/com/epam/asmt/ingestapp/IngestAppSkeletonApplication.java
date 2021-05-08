4049be2b4a323c1c8a62c7b672f55038b3211664
Aleksandr Barmin <barmin.alexander@gmail.com>
2021-05-08 16:04:22 +0300

    Starting the coding task

---
+++ src/main/java/com/epam/asmt/ingestapp/IngestAppSkeletonApplication.java
@@ -0,0 +1,13 @@
+package com.epam.asmt.ingestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngestAppSkeletonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngestAppSkeletonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epam/asmt/ingestapp/IngestAppSkeletonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.asmt.ingestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngestAppSkeletonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
